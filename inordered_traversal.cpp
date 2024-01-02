7abceefa94cf6ba8608cb79b6fbc2e63d76214f3
Sojib Mondol <mdsojibmandol@gmail.com>
2024-01-02 21:00:22 +0600

    inordered traversal coadingninja problem solution in c++

---
+++ inordered_traversal.cpp
@@ -0,0 +1,37 @@
+// https://www.codingninjas.com/studio/problems/inorder-traversal_3839605?leftPanelTabValue=PROBLEM
+// solution in c++
+
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+
+vector<int> v;
+
+void inordered(TreeNode *root)
+{
+    // basecase
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inordered(root->left);
+    v.push_back(root->data);
+    inordered(root->right);
+}
+
+vector<int> getInOrderTraversal(TreeNode *root)
+{
+    v.clear();
+    inordered(root);
+    return v;
+}
