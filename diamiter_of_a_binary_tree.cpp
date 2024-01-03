c2af9bb05e48f946c3a2c05d2d150ba9d8489d5e
Sojib Mondol <mdsojibmandol@gmail.com>
2024-01-04 03:31:58 +0600

    diamiter of a binary tree solution in c++

---
+++ diamiter_of_a_binary_tree.cpp
@@ -0,0 +1,42 @@
+// https://www.codingninjas.com/studio/problems/diameter-of-the-binary-tree_920552
+// Diameter Of Binary Tree solution in c++
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int mx = 0;
+
+int maxHeight(TreeNode<int> *root)
+{
+    if (root == NULL)
+        return 0;
+    int l = maxHeight(root->left);
+    int r = maxHeight(root->right);
+    int d = l + r;
+    mx = max(mx, d);
+    return max(l, r) + 1;
+}
+
+int diameterOfBinaryTree(TreeNode<int> *root)
+{
+    mx = 0;
+    int l = maxHeight(root->left);
+    int r = maxHeight(root->right);
+    return max(mx, l + r);
+}
